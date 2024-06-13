b9367a7f6b9750a173cdfe783409d0592ef81288
ArthurMLi <12200069@aluno.cotemig.com.br>
2024-06-13 14:08:04 -0300

    app principal + teste

---
+++ BlazorApp1/BlazorApp1/Components/Controller/CodigoAleatorio.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Timers;
+
+namespace BlazorApp1.Components.Controller
+{
+	public class CodigoAleatorio
+	{
+
+		public string CriarCodigo()
+		{
+			Random rand = new Random();
+			 string Codigo = "";
+
+			char[] Letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+			int cont = 0;
+			int QuantidadeDeLetras = 6;//Escolhe a quantidade de letras/numeros q quer tenha no codigo
+			while (cont != QuantidadeDeLetras)
+			{
+				if (rand.Next(0, 2) == 0)
+				{
+					Codigo += rand.Next(0, 9);
+				}
+				else
+				{
+					Codigo += Letras[rand.Next(0, Letras.Length)];
+				}
+				cont++;
+			}
+			return Codigo;
+
+		}
+		//so testando aqui v
+		public String VerificadorCodigo()
+		{
+
+			bool teste = false;
+			var timer = new System.Timers.Timer(5);
+			timer.AutoReset = true;
+			timer.Elapsed += (source, eventArgs) =>
+			{
+
+				teste = true;
+
+			};
+			timer.Start();
+			if (teste)
+			{
+				return CriarCodigo();
+			}
+			else return "";
+
+		}
+	}
+}
+		
+
+
+
---
+++ BlazorApp1/BlazorApp1/Components/Controller/NumerosAleatorios.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp1.Components.Controller
+{
+	public class CodigoAleatorio
+	{
+		public int CriarCodigo()
+		{
+			Random rand = new Random();
+
+
+			return 0;//so pra n dar erro por enquanto
+		}
+	}
+}
