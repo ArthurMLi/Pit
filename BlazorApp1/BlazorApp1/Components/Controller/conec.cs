364cb830e5215e5cc7d405acfa71da045d5a84af
ArthurMLi <12200069@aluno.cotemig.com.br>
2024-06-17 21:26:39 -0300

    teste dudu

--- BlazorApp1/BlazorApp1/Components/Controller/conec.cs
+++ BlazorApp1/BlazorApp1/Components/Controller/conec.cs
@@ -12,18 +12,5 @@ using FireSharp.Response;
 
 namespace BlazorApp1.Components.Controller
 {
-    public class conec
-    {
-        IFirebaseConfig config = new FirebaseConfig
-        {
-            AuthSecret = "zHUVhTzDbUSYw1TtgwY3Ro7NPWATxSltlHkA49YP",
-            BasePath = "https://testefila-70e47-default-rtdb.firebaseio.com/"
-
-        };
-        IFirebaseClient cliente;
-        cliente = new FireSharp.FirebaseClient(config);
-        if(cliente){
-            }
-
-    }
+    
 }
