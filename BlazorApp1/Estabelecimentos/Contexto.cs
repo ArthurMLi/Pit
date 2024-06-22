602beec6bb5ea4c571107b452df7caa4ee4ffc84
Luiz Felipe <luizf.a.cavalcante@gmail.com>
2024-06-22 11:37:38 -0300

    arquitetura das classes porém com metodos não desenvolvidos

---
+++ BlazorApp1/Estabelecimentos/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ControleDeEstabelecimentos.Modelos;
+namespace ControleDeEstabelecimentos.Dados
+{
+    internal class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Gerente> Gerentes { get; set; }
+        public DbSet<Estabelecimento> Estabelecimentos { get; set; }
+    }
+}
