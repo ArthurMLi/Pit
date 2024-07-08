64e130b35d137903ee9e2a3ae4107e0191a7d2ea
Luiz Felipe <luizf.a.cavalcante@gmail.com>
2024-07-08 15:33:34 -0300

    prototipo de tela de estabelecimento e avaliação

--- BlazorApp1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4a7678f1ee2d9273ffc164f226db1b331697077")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5686017a1c317cecc74cd4b09ea72c2684e8f89")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
